3fa2fc5054ae33d600f3e25845ef835db0f41798
JoseV-001 <Leaphaz@gmail.com>
2026-03-26 23:19:08 -0300

    feat: implement FIPE API with Redis caching and PostgreSQL integration

---
+++ src/main/java/com/josev001/fipe_search/repository/ReferenciaRepository.java
@@ -0,0 +1,25 @@
+package com.josev001.fipe_search.repository;
+
+import com.josev001.fipe_search.dto.ConsultaFipeDTO;
+import com.josev001.fipe_search.entity.ReferenciaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReferenciaRepository extends JpaRepository<ReferenciaEntity, Long> {
+
+    @Query("""
+            SELECT new com.josev001.fipe_search.dto.ConsultaFipeDTO(
+                ma.nome, mo.nome, r.anoModelo, r.preco, r.mesReferencia)
+            FROM ReferenciaEntity r
+                JOIN r.modelo mo
+                JOIN mo.marca ma
+            WHERE r.modelo.id = :modeloId AND r.anoModelo = :anoModelo
+            ORDER BY r.mesReferencia DESC
+            """)
+    List<ConsultaFipeDTO> findReferencias(
+            @Param("modeloId") Long modeloId,
+            @Param("anoModelo") Integer anoModelo);
+}
